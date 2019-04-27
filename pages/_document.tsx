4a2e8a6f7168f4fb1940b8845fc4e15fe9b36f53
Florentin Rieger <florentin.rieger@gmail.com>
2019-04-27 15:17:44 +0200

    feature: added normalize.css and font

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+// _document is only rendered on the server side and not on the client side
+// Event handlers like onClick can't be added to this file
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class CustomDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="stylesheet" href="/static/styles/normalize.css" />
+          <link rel="stylesheet" href="/static/styles/main.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
