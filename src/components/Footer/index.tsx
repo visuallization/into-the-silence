5cca6c663af8bc8eac637d0b9d9019321fc2f577
Florentin Rieger <florentin.rieger@gmail.com>
2019-05-17 15:37:40 +0200

    feat(footer): added fake email and telephone number

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,10 +8,11 @@ class Footer extends React.Component<{}, {}>{
       <div className={styles.footer}>
         <div className={styles.content}>
           <ul>
-            <li>Footer</li>
-            <li>Some text</li>
-            <li>and</li>
-            <li>another</li>
+            <li>Mag<sup>a</sup>. Heidrun Rieger</li>
+            <li>Hetzendorf, AT</li>
+            <li><a href="tel:+43 0123 456 789">+43 0123 456 789</a></li>
+            <li><a href="mailto:my@email.com">my@email.com</a></li>
+            <li><a>Impressum</a></li>
           </ul>
         </div>
       </div>
