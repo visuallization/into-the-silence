11580e39456b999eb1ef51e6bb28937985bd368d
Florentin Rieger <florentin.rieger@gmail.com>
2019-05-25 17:06:59 +0200

    feat(menu): added scrolling pollyfill to support mobile browsers

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,16 @@
 // _document is only rendered on the server side and not on the client side
 // Event handlers like onClick can't be added to this file
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import smoothscroll from 'smoothscroll-polyfill';
 
 class CustomDocument extends Document {
   static async getInitialProps(ctx: any) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
+  }
+
+  componentDidMount() {
+    smoothscroll.polyfill();
   }
 
   render() {
